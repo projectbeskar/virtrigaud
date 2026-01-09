e9bc654410775c28fdd9173c745c25a51f341cc6
William Rizzo <william.rizzo@gmail.com>
2026-01-09 16:09:30 +0000

    add adoption controller to manager

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -192,6 +192,16 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "VMMigration")
 		os.Exit(1)
 	}
+
+	// Register VMAdoption controller
+	if err = (&controller.VMAdoptionReconciler{
+		Client:         mgr.GetClient(),
+		Scheme:         mgr.GetScheme(),
+		RemoteResolver: remoteResolver,
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "VMAdoption")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
